d6bec2ae0a45798578796970c6d29e0bccffcf80
neal.okelly100@gmail.com <Cra56056!>
2018-11-02 14:37:51 +0000

    Added phase-banner position

--- templates/govuk_template/index.php
+++ templates/govuk_template/index.php
@@ -72,18 +72,8 @@
       </div>
     </div>
   </header>
-
-  <div class="govuk-width-container">
-    <div class="govuk-phase-banner">
-      <p class="govuk-phase-banner__content"><strong class="govuk-tag govuk-phase-banner__content__tag ">
-  alpha
-</strong>
-        <span class="govuk-phase-banner__text">
-          This is a new service – your
-          <a class="govuk-link" href="#">feedback</a> will help us to improve it.
-        </span>
-      </p>
-    </div>
+	<div class="govuk-width-container">
+	<jdoc:include type="modules" name="phase-banner" />
 	<jdoc:include type="modules" name="breadcrumbs" />
 									   
     <main class="govuk-main-wrapper app-main-class" id="main-content" role="main">
